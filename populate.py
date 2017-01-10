6c36262061296d5ac2c65c8e59ffd4f775d670fc
Pritheesh Panchmahalkar <pritheesh@Pritheeshs-MacBook-Air.local>
2017-01-10 22:45:21 +0530

    First commit

--- populate.py
+++ populate.py
@@ -29,29 +29,31 @@ for i in range(0, 6):
             father_name = sheet.cell(row, 9).value
             father_mobile = str(int(sheet.cell(row, 11).value))
             values = (father_name.strip(), father_mobile.strip())
-            print values
+            # print values
             cursor.execute(query1, values)
         except:
             pass
 
 db.commit()
-
-for row in range(5, sheet.nrows):
-    try:
-        father_name = sheet.cell(row, 9).value.strip()
-        father_mobile = str(int(sheet.cell(row, 11).value)).strip()
-        hall_ticket = sheet.cell(row, 1).value.strip()
-        name = sheet.cell(row, 2).value.strip()
-        gender = str(int(sheet.cell(row, 8).value)).strip()
-        mother_name = sheet.cell(row, 10).value.strip()
-        student_mobile = str(int(sheet.cell(row, 12).value)).strip()
-        email = str(sheet.cell(row, 13).value).strip()
-        object = Parent.objects.filter(mobile=father_mobile)[0]
-        stud = Student(name=name, hall_ticket=hall_ticket, gender=gender, mother_name=mother_name, father_name=father_name,
-                       student_mobile=student_mobile, email=email, parent_mobile=father_mobile, parent_id=object)
-        stud.save()
-    except:
-        pass
+for i in range(0, 6):
+    sheet = book.sheet_by_index(i)
+    for row in range(5, sheet.nrows):
+        try:
+            father_name = sheet.cell(row, 9).value.strip()
+            father_mobile = str(int(sheet.cell(row, 11).value)).strip()
+            hall_ticket = sheet.cell(row, 1).value.strip()
+            print hall_ticket
+            name = sheet.cell(row, 2).value.strip()
+            gender = str(int(sheet.cell(row, 8).value)).strip()
+            mother_name = str(sheet.cell(row, 10).value).strip()
+            student_mobile = str(int(sheet.cell(row, 12).value)).strip()
+            email = str(sheet.cell(row, 13).value).strip()
+            object = Parent.objects.filter(mobile=father_mobile)[0]
+            stud = Student(name=name, hall_ticket=hall_ticket, gender=gender, mother_name=mother_name, father_name=father_name,
+                           student_mobile=student_mobile, email=email, parent_mobile=father_mobile, parent_id=object)
+            stud.save()
+        except:
+            pass
 
 book = xlrd.open_workbook("results.xls")
 sheet = book.sheet_by_name("TSheet")
@@ -65,23 +67,26 @@ for row in range(4, sheet.nrows):
         sub.save()
 
 for row in range(4, sheet.nrows):
-    hall_ticket = sheet.cell(row, 0).value.strip()
-    print hall_ticket
-    stud = Student.objects.get(hall_ticket=hall_ticket)
-    sub_code = sheet.cell(row, 2).value.strip()
-    sub = Subject.objects.get(subject_code=sub_code)
     try:
-        int_marks = str(int(sheet.cell(row, 4).value))
-    except:
-        int_marks = str(sheet.cell(row, 4).value)
-    try:
-        ext_marks = str(int(sheet.cell(row, 5).value))
-    except:
-        ext_marks = str(sheet.cell(row, 5).value)
+        hall_ticket = sheet.cell(row, 0).value.strip()
+        print hall_ticket
+        stud = Student.objects.get(hall_ticket=hall_ticket)
+        sub_code = sheet.cell(row, 2).value.strip()
+        sub = Subject.objects.get(subject_code=sub_code)
+        try:
+            int_marks = str(int(sheet.cell(row, 4).value))
+        except:
+            int_marks = str(sheet.cell(row, 4).value)
+        try:
+            ext_marks = str(int(sheet.cell(row, 5).value))
+        except:
+            ext_marks = str(sheet.cell(row, 5).value)
 
-    res = sheet.cell(row, 7).value
-    credits = int(sheet.cell(row, 8).value)
+        res = sheet.cell(row, 7).value
+        credits = int(sheet.cell(row, 8).value)
 
-    result = Result(hall_ticket=stud, subject_code=sub, internal_marks=int_marks, external_marks=ext_marks, results=res,
-                    credits=credits)
-    result.save()
+        result = Result(hall_ticket=stud, subject_code=sub, internal_marks=int_marks, external_marks=ext_marks, results=res,
+                        credits=credits)
+        result.save()
+    except:
+        pass
